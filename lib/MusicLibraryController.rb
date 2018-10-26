403425602c305561de40a9db4b9394d7c0961559
Austin  Burke <aburk3@gmail.com>
2018-10-26 11:49:19 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -24,10 +24,21 @@ class MusicLibraryController
 
       input = gets.chomp
 
-      case input 
+      case input
       when "list songs"
         list_songs
-      when
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "list artist"
+        list_songs_by_artist
+      when "list genre"
+        list_songs_by_genre
+      when "play song"
+        play_song
+      end
+      
     end
   end
 
